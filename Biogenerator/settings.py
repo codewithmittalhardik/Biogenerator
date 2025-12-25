653cacd010f41330c3c829b08c00f4f16825aec7
hardikmittal23 <106722937+hardikmittal23@users.noreply.github.com>
2025-12-25 21:11:58 +0530

    set

--- Biogenerator/settings.py
+++ Biogenerator/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 # settings.py
 
 # Replace 'your-app-name' with your actual Render project name
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
